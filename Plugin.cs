d24606fee384b268a5d75ce2eb4894a60d2d9444
MagincyanGames <77320870+MagincyanGames@users.noreply.github.com>
2025-02-15 23:39:43 +0100

    setting up the template

---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using CommandTerminal;
+using Farlands.Inventory;
+using Farlands.PlantSystem;
+using FarlandsCoreMod;
+using FarlandsCoreMod.Extensors;
+using System.Diagnostics;
+using System.Linq;
+
+using FarlandsCoreMod.Utiles.Sprites;
+using UnityEngine;
+namespace FrancopetaMod
+{
+    [BepInPlugin("YOUR GUID", "YOUR MOD", "YOUR VERSION")]
+    public class TemplateMod : AbstractMod
+    {
+        public override Sprite LoadSprite(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
